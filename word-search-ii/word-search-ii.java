b95efb9599383036019479a39bd47263abcb9aa0
Itibansal16 <55867708+Itibansal16@users.noreply.github.com>
2021-06-30 08:03:30 -0400

    Time: 1645 ms (12.35%), Space: 40 MB (14.49%) - LeetHub

--- word-search-ii/word-search-ii.java
+++ word-search-ii/word-search-ii.java
@@ -1,53 +1,45 @@
-class TrieNode{
-    String word;
-    TrieNode[] children;
-    boolean isTerminating;
-    public TrieNode(){
-        children = new TrieNode[26];
-        word = "";
-        isTerminating = false;
-    }
-    public void insert(TrieNode root, char[] word, int i){
-        if(word.length == i){
-            root.word = new String(word);
-            root.isTerminating = true;
-            return;
-        }
-        if(root.children[word[i] - 'a'] == null){
-            root.children[word[i] - 'a'] = new TrieNode();
-        }
-        insert(root.children[word[i] - 'a'], word, i+1);
-    }
-}
 class Solution {
+    Set<String> givenWords;
+    Set<String> prefix;
     Set<String> ans;
-    private void dfs(char[][] board, int i, int j, TrieNode root){
-        if(i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] == '#'){
+    boolean[][] visited;
+    private void dfs(char[][] board, int i, int j, String currWord){
+        if(givenWords.contains(currWord)){
+            ans.add(currWord);
+           // return;
+        }
+        if(i < 0 || j < 0 || i >= board.length || j >= board[0].length || visited[i][j]){
+            
             return;
         }
-        TrieNode child = root.children[board[i][j] - 'a'];
-        if(child == null) return;
-        if(child.isTerminating){
-            ans.add(child.word);
-        }
-              
-        char temp = board[i][j];
-        board[i][j] = '#';
-        dfs(board, i+1, j, child);
-        dfs(board, i-1, j, child);
-        dfs(board, i, j+1, child);
-        dfs(board, i, j-1, child);
-        board[i][j] = temp;
+        
+       // System.out.println(currWord + " " + i + " " + j);
+        if(!prefix.contains(currWord)) return;
+        char c = board[i][j];
+        visited[i][j] = true;
+        dfs(board, i+1, j, currWord+c);
+        dfs(board, i-1, j, currWord+c);
+        dfs(board, i, j+1, currWord+c);
+        dfs(board, i, j-1, currWord+c);
+        visited[i][j] = false;
     }
+    
     public List<String> findWords(char[][] board, String[] words) {
-        TrieNode root = new TrieNode();
+        visited = new boolean[board.length][board[0].length];
+        
+        givenWords = new HashSet();
+        prefix = new HashSet();
         ans = new HashSet();
-        for(String word : words){
-            root.insert(root, word.toCharArray(), 0);
+        prefix.add("");
+        for(String w : words) givenWords.add(w);
+        for(String w : words){
+            for(int i = 0; i < w.length(); i++){
+                prefix.add(w.substring(0,i));
+            }
         }
         for(int i = 0; i < board.length; i++){
             for(int j = 0; j < board[0].length; j++){
-                dfs(board, i, j, root);
+                dfs(board, i, j, "");
             }
         }
         return new ArrayList(ans);
