e0186c7b207aa90a747ee755c184fcd0c3d14946
Itibansal16 <55867708+Itibansal16@users.noreply.github.com>
2021-07-26 01:35:15 -0400

    Update jump-game-vi.java

--- jump-game-vi/jump-game-vi.java
+++ jump-game-vi/jump-game-vi.java
@@ -1,26 +1,24 @@
 /*
   O(NK): dp[i] will store the maximum score to reach till i
-         for every i, check for max at i+1,i+2,...,i+k
-         dp[i] = nums[i] + max;
-  It can be observed that for every window of size k, we need the maximum. So this problem is similar to sliding window maximum       
+         It can be observed that for every window of size k, we need the maximum. So this problem is similar to sliding window maximum       
 */
 class Solution {
     public int maxResult(int[] nums, int k) {
-		Deque<Integer> q = new LinkedList<Integer>();
-		for (int i = 0; i < nums.length; i++) {
-            //update nums[i] value with maximum score to reach till i
-            int max = q.size() == 0 ? 0 : nums[q.peekFirst()];
-            nums[i] += max;
+	    Deque<Integer> q = new LinkedList<Integer>();
+	    for (int i = 0; i < nums.length; i++) {
+		    //update nums[i] value with maximum score to reach till i
+		    int max = q.size() == 0 ? 0 : nums[q.peekFirst()];
+		    nums[i] += max;
+
+		    //validate the window of size k
+		    if(q.size() > 0 && i - q.peek() + 1 > k) q.pollFirst();
+
+		    //remove minimum values
+		    while(q.size() > 0 && nums[q.peekLast()] < nums[i]) q.pollLast();
+
+		    q.offer(i);
             
-            //validate the window of size k
-			if(q.size() > 0 && i - q.peek() + 1 > k) q.pollFirst();
-            
-            //remove minimum values
-            while(q.size() > 0 && nums[q.peekLast()] < nums[i]) q.pollLast();
-            
-            q.offer(i);
-            
-		}
-		return nums[nums.length - 1];
-	}
+	    }
+	    return nums[nums.length - 1];
+    }
 }
