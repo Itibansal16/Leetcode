5a2a1dbd5590612edd703c6c70ab8d1069467bf8
Itibansal16 <55867708+Itibansal16@users.noreply.github.com>
2021-08-15 19:35:11 +0530

    Time: 0 ms (100.00%), Space: 37.6 MB (80.03%) - LeetHub

--- binary-tree-right-side-view/binary-tree-right-side-view.java
+++ binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -1,17 +1,17 @@
 class Solution {
-    private void helper(TreeNode root, int level, List<Integer> ans){
-        if(root == null) return;
-        
-        if(level == ans.size()){
-            ans.add(root.val);
+    private void helper(TreeNode root, int level, List<Integer> rightView){
+        if(root == null){
+            return;
         }
-        
-        helper(root.right, level + 1, ans);
-        helper(root.left, level + 1, ans);
+        if(level == rightView.size()){
+            rightView.add(root.val);
+        }
+        helper(root.right, level + 1, rightView);
+        helper(root.left, level + 1, rightView);
     }
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> ans = new ArrayList();
-        helper(root, 0, ans);
-        return ans;
+        List<Integer> rightView = new ArrayList();
+        helper(root, 0, rightView);
+        return rightView;
     }
 }
